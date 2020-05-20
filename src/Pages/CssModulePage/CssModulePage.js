ee723d93a9b95f1e9b18877fe6c7f342542bb48d
AnyaaBond <psela1999@gmail.com>
2020-05-20 13:51:36 +0400

    Сделала 7 домашнюю (наверное)

--- src/Pages/CssModulePage/CssModulePage.js
+++ src/Pages/CssModulePage/CssModulePage.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import css from './CssModulePage.module.css'
 import Button from './components/Button/Button'
+import Input from './components/Input/Input'
 
 function CssModulePage () {
   return (
     <div className={css.wrapper}>
       <div className={css.list}>
-        <div className={css.item}>
-          <Button className={css.btn} isRed label='Кнопка' />
-        </div>
+        <Button className={css.btn} isRed label='Кнопка' />
+        <Input className={css.inp} isWide label='Инпут:' />
       </div>
     </div>
   )
---
+++ src/Pages/CssModulePage/components/Input/Input.js
@@ -0,0 +1,40 @@
+import css from './Input.module.scss'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import React, { useState } from 'react'
+
+function Input ({ className, isWide, label }) {
+  const [isFocused, setIsFocused] = useState(false)
+  const [isHovered, setIsHovered] = useState(false)
+
+  const inpClass = classNames(
+    css.inp,
+    className,
+    {
+      isWide,
+      'inp-focused': isFocused,
+      'inp-hover': !isFocused && isHovered
+    }
+  )
+
+  return (
+    <div>
+      <label className='label'>{label}</label>
+      <input
+        className={inpClass}
+        onFocus={() => { setIsFocused(true) }}
+        onBlur={() => { setIsFocused(false) }}
+        onMouseEnter={() => { setIsHovered(true) }}
+        onMouseLeave={() => { setIsHovered(false) }}
+      />
+    </div>
+  )
+}
+
+Input.propTypes = {
+  className: PropTypes.string,
+  isWide: PropTypes.bool,
+  label: PropTypes.string
+}
+
+export default Input
