d5a8d8369eb09c72032a66f1095558f9c0ad816c
AnyaaBond <psela1999@gmail.com>
2020-05-20 14:22:23 +0400

    правка 7-ого дз

--- src/Pages/CssModulePage/components/Input/Input.js
+++ src/Pages/CssModulePage/components/Input/Input.js
@@ -11,7 +11,7 @@ function Input ({ className, isWide, label }) {
     css.inp,
     className,
     {
-      isWide,
+      [css.wide]: isWide,
       'inp-focused': isFocused,
       'inp-hover': !isFocused && isHovered
     }
