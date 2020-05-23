f8898669a3311461dbb4cc339fe569ec87118c4a
AnyaaBond <psela1999@gmail.com>
2020-05-23 12:11:53 +0400

    10 домашняя!

--- src/Pages/PageList/PageList.js
+++ src/Pages/PageList/PageList.js
@@ -3,10 +3,29 @@ import data from './assets/data.js'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Button from 'react-bootstrap/Button'
 import { Link } from 'react-router-dom'
+import css from '../../components/Menu/Menu.module.scss'
 
 function PageList () {
   const { list: dataList } = data
   const [list, setList] = useState(dataList)
+
+  const renderButLink = ({ list }) => {
+    if (list.length > 4) {
+      return (
+        <div><Link to='/' activeClassName={css.active} exact>Посмотреть все новости</Link></div>
+      )
+    } else {
+      return (
+        <Button
+          onClick={() => {
+            setList([...list, ...data.list])
+          }}
+        >
+          Показать еще
+        </Button>
+      )
+    }
+  }
   return (
     <>
       <h1>Список</h1>
@@ -18,13 +37,7 @@ function PageList () {
           </ListGroup.Item>
         ))}
       </ListGroup>
-      <Button
-        onClick={() => {
-          setList([...list, ...data.list])
-        }}
-      >
-        Показать еще
-      </Button>
+      {renderButLink({ list })}
     </>
   )
 }
